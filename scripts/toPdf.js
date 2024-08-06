49d35a0861ce5664941b481822c8a7a7c09d082b
nicolastorre <10921378+nicolastorre@users.noreply.github.com>
2024-08-06 16:47:23 +0200

    init React
    POC to generate pdf from react website

---
+++ scripts/toPdf.js
@@ -0,0 +1,14 @@
+const puppeteer = require("puppeteer");
+
+async function init() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000", {
+    waitUntil: "networkidle2",
+  });
+  await page.pdf({ path: "output.pdf", format: "A4", printBackground: true });
+
+  await browser.close();
+}
+
+init();
