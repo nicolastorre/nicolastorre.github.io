a7420538ee095374be4b8720c1e6c3b887a1aa0d
nicolastorre <10921378+nicolastorre@users.noreply.github.com>
2024-08-14 17:35:39 +0200

    fix generate pdf

--- scripts/pdf.js
+++ scripts/pdf.js
@@ -2,7 +2,6 @@ const puppeteer = require("puppeteer");
 
 const args = process.argv.slice(2);
 const CV_URL = args[0] || "http://localhost:3000";
-console.log(CV_URL);
 
 async function generatePDF(url) {
   const browser = await puppeteer.launch();
@@ -11,7 +10,7 @@ async function generatePDF(url) {
     waitUntil: "networkidle2",
   });
   await page.pdf({
-    path: "cv_en.pdf",
+    path: "cv.pdf",
     format: "A4",
     printBackground: true,
     landscape: false,
