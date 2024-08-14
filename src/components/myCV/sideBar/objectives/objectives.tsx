f2316887c4b15711d800dcbbb41e38d3d420b73d
nicolastorre <10921378+nicolastorre@users.noreply.github.com>
2024-08-14 14:19:43 +0200

    Fix tel + margin objectives title

--- src/components/myCV/sideBar/objectives/objectives.tsx
+++ src/components/myCV/sideBar/objectives/objectives.tsx
@@ -7,7 +7,7 @@ type ObjectivesProps = {
 const Objectives = ({ objectivesData }: ObjectivesProps) => {
   return (
     <div className="p-4">
-      <h3 className="text-black font-bold text-center">Objectives</h3>
+      <h3 className="text-black font-bold text-center mb-2">Objectives</h3>
       <p className="text-black text-sm text-justify">{objectivesData}</p>
     </div>
   );
