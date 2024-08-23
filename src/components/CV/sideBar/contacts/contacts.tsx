482c8a85184eb45156558506b41485df1cefc440
nicolastorre <10921378+nicolastorre@users.noreply.github.com>
2024-08-23 08:13:23 +0200

    Remove phone contact

--- src/components/CV/sideBar/contacts/contacts.tsx
+++ src/components/CV/sideBar/contacts/contacts.tsx
@@ -1,14 +1,20 @@
 import { IconName, IconPrefix } from "@fortawesome/fontawesome-svg-core";
 import { CVData } from "../../../../interface/CVData";
 import DynamicIcon from "../../../dynamicIcon/DynamicIcon";
+import { useTranslation } from "react-i18next";
 
 type ContactsProps = {
   contactsData: CVData["contacts"];
 };
 
 const Contacts = ({ contactsData }: ContactsProps) => {
+  const { t } = useTranslation();
+
   return (
-    <>
+    <div className="p-4">
+      <h3 className="text-black font-bold underline text-center mb-2">
+        {t("CONTACTS")}
+      </h3>
       {contactsData ? (
         <div className="p-4 m-2 flex flex-col justify-center items-center">
           {contactsData.map((contact) => (
@@ -26,7 +32,7 @@ const Contacts = ({ contactsData }: ContactsProps) => {
           ))}
         </div>
       ) : null}
-    </>
+    </div>
   );
 };
 
