4d49c1a30efe7c747cae5596f4f5fdad0ebd85c7
nicolastorre <10921378+nicolastorre@users.noreply.github.com>
2024-09-16 16:51:25 +0200

    fix style font size

--- src/components/CV/sideBar/personalDetails/personalDetails.tsx
+++ src/components/CV/sideBar/personalDetails/personalDetails.tsx
@@ -15,7 +15,7 @@ const PersonalDetails = ({ personalDetailsData }: PersonalDetailsProps) => {
       </h3>
       {Object.keys(personalDetailsData).map((key: string) => (
         <div className="flex flex-row justify-between mb-2">
-          <p className="text text-sm capitalize font-bold mr-2">{key}:</p>
+          <p className="text text-xs capitalize font-bold mr-2">{key}:</p>
           <p className="text text-xs text-right">
             {personalDetailsData[key as keyof CVData["personalDetails"]]}
           </p>
